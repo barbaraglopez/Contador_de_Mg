bc8f8b8c1abf19697d6780270b1e641b9f21aa62
Barbara <barbaralopezbbbb@gmail.com>
2021-11-18 12:37:59 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const btnMas = document.querySelector('#btn-m')
+const btnMenos = document.querySelector('#btn')
+const meGusta = document.getElementById('contadorPositivo')
+const noMeGusta = document.getElementById('contadorNegativo')
+
+const like = () => {
+    
+}
+
+btnMas.onclick = () =>{
+    let contador = meGusta.innerText
+    contador++
+    meGusta.innerText = contador
+}
+
+btnMenos.onclick = () =>{
+    let contadorNegativo = noMeGusta.innerText
+    contadorNegativo++
+    noMeGusta.innerText = contadorNegativo
+}
